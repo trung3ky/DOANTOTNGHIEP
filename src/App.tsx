79cb13be9cc5e4f4fed09d964e59105f7979a540
@trunghq <trunghq@fiisoft.com>
2022-05-13 23:22:56 +0700

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Login } from "./module/account";
+import "./App.scss";
+
+const Home = () => {
+  return (
+    <ul>
+      <li>
+        <Link to="/login" className="nav-link">
+          Đăng nhập
+        </Link>
+      </li>
+      <li>
+        <Link to="/register" className="nav-link">
+          Đăng ký
+        </Link>
+      </li>
+    </ul>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Home />} />
+      </Routes>
     </div>
   );
 }
